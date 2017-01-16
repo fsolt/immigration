213c1ec20545dceec1ea3b0a4768694761516d2b
Frederick Solt <fsolt@mac.com>
2017-01-16 10:22:48 -0600

    use DCPO package

---
+++ R/processing.R
@@ -0,0 +1,13 @@
+library(DCPO)
+library(tidyverse)
+library(stringr)
+
+
+im <- dcpo_setup(vars = "data-raw/immigration_vars.csv",
+                 file = "data/immigration_vars.csv")
+
+im <- read_csv("data/immigration_vars.csv")
+
+x <- im %>% with_min_yrs(3)
+out1 <- dcpo(x, iter = 4000)
+save(out1, file = str_c("results/gm_", str_replace(Sys.time(), " ", "_"), ".rda"))
