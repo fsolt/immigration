0b2f649fa6a1c557cac7d3c9c06949a87831f45e
Frederick Solt <frederick-solt@uiow.edu>
2017-01-20 09:47:22 -0600

    use data dir; use countries with >= 5 years data

--- R/processing.R
+++ R/processing.R
@@ -1,6 +1,6 @@
-library(DCPO)
 library(tidyverse)
 library(stringr)
+library(DCPO)
 
 
 im <- dcpo_setup(vars = "data-raw/immigration_vars.csv",
@@ -8,6 +8,6 @@ im <- dcpo_setup(vars = "data-raw/immigration_vars.csv",
 
 im <- read_csv("data/immigration_vars.csv")
 
-x <- im %>% with_min_yrs(3)
-out1 <- dcpo(x, iter = 4000)
-save(out1, file = str_c("results/im_", str_replace(Sys.time(), " ", "_"), ".rda"))
+x <- im %>% with_min_yrs(5)
+out1 <- dcpo(x, iter = 400)
+save(out1, file = str_c("data/im_", str_replace(Sys.time(), " ", "_"), ".rda"))
